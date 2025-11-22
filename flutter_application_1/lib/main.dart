18bc6068d07afc1cf47b85e32e99bed547c59853
Mahmoud-Mohamad21 <mahmoud1mohamad2005@gmail.com>
2025-11-23 00:27:47 +0200

    finalize the main.dart file

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Currency Converter Application',
+     title: 'Currency Converter Application',
       home: Home(), 
     );
   }
